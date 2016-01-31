dcbc97398ca0f049891da8f7b9aa299f17e02883
hondax <dannyxiao16@gmail.com>
2016-01-31 11:42:37 -0800

    Create motor.hpp

---
+++ motor.hpp
@@ -0,0 +1,14 @@
+#ifndef MOTORS_H
+#define MOTORS_H
+
+class Motor {
+  private:
+    int pin1_, pin2_, pin_pwm_;
+  public:
+    Motor(int pin1, int pin2, int pwm_pin);
+    void Set(float acceleration, float curr_velocity);
+};
+
+extern Motor motor_l, motor_r;
+
+#endif
