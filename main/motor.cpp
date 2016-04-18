6e3b6e41a124a73f33a0c8841b57ce12176b56ce
hondax <hoxiao@ucsd.edu>
2016-04-18 15:09:14 -0700

    "Modified turn methods in main and the motor class."

--- main/motor.cpp
+++ main/motor.cpp
@@ -1,44 +1,4 @@
 #include "motor.h"
-
-bool locked = false;
-
-
-
-
-
-
-
-Ticker motorInOperation; //Ticker to for async driving
-
-
-void stop(){
-		leftMotor.stop();
-		rightMotor.stop();
-}
-
-
-void turn(float speed) {
-		leftMotor.speed(speed);
-		rightMotor.speed(-speed);
-}
-
-void drive(float speed){
-		leftMotor.speed(speed);
-		rightMotor.speed(speed);
-}
-
-
-
-
-void drive(float speed, int distance, void(*callback)(void)){
-        resetEncoders();
-        DriveLock lock (distance, speed, callback);
-        drive(speed);
-        motorInOperation.attach(&lock, &DriveLock::drive, 0.001); //Checks every milisecond
-}
-
-
-
 //Defines a motor and its basic methods
 Motor::Motor(PinName _pwm, PinName _fwd, PinName _rev):
         pwm(_pwm), fwd(_fwd), rev(_rev) {
@@ -56,7 +16,5 @@ void Motor::speed(float speed) {
 void Motor::stop() {
     speed(0.0);
 }
-
-
 
 
